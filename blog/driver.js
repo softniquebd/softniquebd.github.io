f69c697400a977d2ac297508c95a3a2189f00dbc
shuvo18103107 <18103107@iubat.edu>
2021-03-16 16:34:11 +0600

    json fetch work

--- blog/driver.js
+++ blog/driver.js
@@ -1,47 +1,10 @@
-import data from './data.json';
-console.log(data);
-
-
-
-` <div class="blog">
-<div class="container">
-    <div class="owl-carousel owl-theme blog-post">
-        <div class="blog-content" data-aos="fade-right" data-aos-delay="200">
-            <img src="../assets/Blog-post/post-1.jpg" alt="post-1">
-            <div class="blog-title">
-                <h3>London Fashion week's continued the evolution</h3>
-                <button class="btn btn-blog theme_btn">Fashion</button>
-                <span>2 minutes</span>
-            </div>
-        </div>
-        <div class="blog-content" data-aos="fade-in" data-aos-delay="200">
-            <img src="../assets/Blog-post/post-3.jpg" alt="post-1">
-            <div class="blog-title">
-                <h3>London Fashion week's continued the evolution</h3>
-                <button class="btn btn-blog theme_btn">Fashion</button>
-                <span>2 minutes</span>
-            </div>
-        </div>
-        <div class="blog-content" data-aos="fade-left" data-aos-delay="200">
-            <img src="../assets/Blog-post/post-2.jpg" alt="post-1">
-            <div class="blog-title">
-                <h3>London Fashion week's continued the evolution</h3>
-                <button class="btn btn-blog theme_btn">Fashion</button>
-                <span>2 minutes</span>
-            </div>
-        </div>
-        <div class="blog-content" data-aos="fade-right" data-aos-delay="200">
-            <img src="../assets/Blog-post/post-5.png" alt="post-1">
-            <div class="blog-title">
-                <h3>London Fashion week's continued the evolution</h3>
-                <button class="btn btn-blog theme_btn">Fashion</button>
-                <span>2 minutes</span>
-            </div>
-        </div>
-    </div>
-    <div class="owl-navigation" id="PostFeed">
-        <span class="owl-nav-prev"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></span>
-        <span class="owl-nav-next"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
-    </div>
-</div>
-</div>`
+$(document).ready(function () {
+    $.getJSON("data.json", function (data) {
+        console.log(data.posts[0]); // Prints: Harry
+        // console.log(data.age); // Prints: 14
+    }).fail(function () {
+        console.log("An error has occurred.");
+    });
+    if (window.location.href.split("?")[1])
+   
+});
