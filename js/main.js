8ea8d7abad0fbd7ab77a4a337e7b83a5202c8d96
shuvo18103107 <18103107@iubat.edu>
2021-03-22 00:56:32 +0600

    messenger api plugin test

--- js/main.js
+++ js/main.js
@@ -160,29 +160,29 @@ if ($('.screenshot-carousel').length) {
   });
 }
 
-// chatbox js part
-(function () {
-  var options = {
-    facebook: '114263207095888', // Facebook page ID
-    whatsapp: '+8801784215213', // WhatsApp number
-    call_to_action: '', // Call to action
-    button_color: '#932C8B', // Color of button
-    position: 'right', // Position may be 'right' or 'left'
-    order: 'facebook,whatsapp', // Order of buttons
-  };
-  var proto = document.location.protocol,
-    host = 'getbutton.io',
-    url = proto + '//static.' + host;
-  var s = document.createElement('script');
-  s.type = 'text/javascript';
-  s.async = true;
-  s.src = url + '/widget-send-button/js/init.js';
-  s.onload = function () {
-    WhWidgetSendButton.init(host, proto, options);
-  };
-  var x = document.getElementsByTagName('script')[0];
-  x.parentNode.insertBefore(s, x);
-})();
+// // chatbox js part
+// (function () {
+//   var options = {
+//     facebook: '114263207095888', // Facebook page ID
+//     whatsapp: '+8801784215213', // WhatsApp number
+//     call_to_action: '', // Call to action
+//     button_color: '#932C8B', // Color of button
+//     position: 'right', // Position may be 'right' or 'left'
+//     order: 'facebook,whatsapp', // Order of buttons
+//   };
+//   var proto = document.location.protocol,
+//     host = 'getbutton.io',
+//     url = proto + '//static.' + host;
+//   var s = document.createElement('script');
+//   s.type = 'text/javascript';
+//   s.async = true;
+//   s.src = url + '/widget-send-button/js/init.js';
+//   s.onload = function () {
+//     WhWidgetSendButton.init(host, proto, options);
+//   };
+//   var x = document.getElementsByTagName('script')[0];
+//   x.parentNode.insertBefore(s, x);
+// })();
 // our work process js
 var elementOld = null;
 var openClass = "Accordion__tab--open";
