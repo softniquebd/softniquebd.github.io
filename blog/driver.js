b4ec6fd05b7bd06ca05c9d8c1ac2ee56433201eb
Passworderror29 <iflamegooglic@gmail.com>
2021-04-16 18:12:22 +0600

    medium data driver js updates - 0

--- blog/driver.js
+++ blog/driver.js
@@ -36,6 +36,7 @@ $.getJSON("data.json", async function (info) {
     var loaction = window.location.href.split("#")[0].split("?")[0];
     var query = url.split("?")[1];
     var data = info.posts;
+    var mediumData;
     var totalPosts = data.length;
     var categories = [];
     var catpostId = [];
@@ -84,20 +85,58 @@ $.getJSON("data.json", async function (info) {
         return listHtml;
     }
 
+    function generateURL() {
+        var urlToPass = 'https://v1.nocodeapi.com/softniquebd_0/medium/huCVrlYIYWadJpFh?'
+        // var URLlist =
+        //     [   'https://v1.nocodeapi.com/softniquebd_0/medium/huCVrlYIYWadJpFh?',
+        //         'https://v1.nocodeapi.com/softniquebd_1/medium/IRXNMmLYulcsChUo?',
+        //         'https://v1.nocodeapi.com/softniquebd_2/medium/cGUQtNTcsWhkcdzm?',
+        //         'https://v1.nocodeapi.com/softniquebd_3/medium/JMiuVHRoyiGcInCz?',
+        //         'https://v1.nocodeapi.com/softniquebd_4/medium/qqOHryMyXAyHAunE?',
+        //         'https://v1.nocodeapi.com/softniquebd_5/medium/zdMkBmHSGKlRRHww?',
+        //         'https://v1.nocodeapi.com/softniquebd_6/medium/iCHMlnvzXXkGTuJI?',
+        //         'https://v1.nocodeapi.com/softniquebd_7/medium/PTocqYFkzbQmHMmV?',
+        //         'https://v1.nocodeapi.com/softniquebd_8/medium/MgQzLKaRiDJLxPZN?',
+        //         'https://v1.nocodeapi.com/softniquebd_9/medium/CytJZVeYLhIemECg?'
+        //     ]
+        // var urlToPass
+
+        // if ( localStorage.getItem("apiUrl") !== null) {
+        //     var urlNum = parseInt(localStorage.getItem("apiUrl")[localStorage.getItem("apiUrl").lastIndexOf("_") + 1]);
+        //     if (urlNum == (URLlist.length()-1)) { urlNum = -1 }
+        //     console.log(urlNum)
+        //     localStorage.setItem("apiUrl", URLlist[urlNum + 1]);
+        //     console.log('localStorage.getItem("apiUrl"): '+localStorage.getItem("apiUrl"))
+        //     urlToPass = URLlist[urlNum + 1];
+        // }
+        // else {
+        //     localStorage.setItem("apiUrl", URLlist[0])
+        //     urlToPass = URLlist[0];
+        // }
+        return urlToPass;
+    }
+
     async function generateDesc(i) {
         var str = 'content:encoded';
+        var streamUrl = generateURL();
+        console.log("url: "+streamUrl)
         var settings = {
-            "url": "https://medium.com/feed/@18103107?format=json",
+            "url": streamUrl,
             "method": "get",
             "timeout": 0,
             "headers": {
                 "Content-Type": "application/json"
             },
         };
-        var content = 'loading..'
+        var content;
         await $.ajax(settings).done(function (response) {
             response = JSON.parse(response);
+            mediumData = response;
+            console.log(mediumData[i].title)
             content = response[parseInt(i)][str];
+        }).fail(function (response) {
+            content = response;
+            console.log(response)
         });
 
         $('.rich-editor-text').append(content);
@@ -108,9 +147,10 @@ $.getJSON("data.json", async function (info) {
             $(this).css("list-style-type", "square");
         });
 
-        return $('.rich-editor-text').contents().filter(function(){
+         $('.rich-editor-text').contents().filter(function(){
             return this.nodeType === 3;
-        }).remove();
+         }).remove();
+        return 'Loading..'
     }
 
     function blogdetails(postnum) {
@@ -129,9 +169,7 @@ $.getJSON("data.json", async function (info) {
                 <div class="cs-post-title">
                     <div class="cs-author">
                         <figure>
-                            <a href="${data[index].gitlink}"><img width="32" height="32"
-                                    srcset="http://1.gravatar.com/avatar/7a20fad302fc2dd4b4649dc5bdb3c463?s=64&amp;d=mm&amp;r=g 2x"
-                                    src="${data[index].writerPic}" alt="${index}"></a>
+                            <a href="${data[index].gitlink}"><img width="32" height="32" src="${data[index].writerPic}" alt="${index}"></a>
                         </figure>
                         <div class="cs-text">
                             <a href="${data[index].gitlink}">${data[index].blogWriter}
@@ -145,7 +183,7 @@ $.getJSON("data.json", async function (info) {
                 </div>
                 <div class="cs-post-option-panel">
                     <div class="rich-editor-text">
-                         ${ generateDesc(index)}
+                         Loading.. ${ generateDesc(index)}
                     </div>
                 </div>
 
